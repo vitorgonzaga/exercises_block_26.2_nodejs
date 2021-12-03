4cf8c9988b2fe793a0830132f7215fa1201a3f05
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-12-03 15:19:54 -0300

    starting exercises of 26.2

---
+++ index.js
@@ -0,0 +1,42 @@
+const myFunction = (param1, param2, param3) => {
+  return new Promise((resolve, reject) => {
+    if (
+      typeof param1 !== "number" ||
+      typeof param2 !== "number" ||
+      typeof param3 !== "number"
+    )
+      reject("Informe apenas números");
+
+    const result = (param1 + param2) * param3;
+
+    if (result < 50) {
+      return reject("Valor muito baixo");
+    }
+
+    resolve(result);
+  });
+};
+
+myFunction(10, 10, 10)
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
+
+myFunction(1, 1, "a")
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
+
+myFunction(1, 1, 1)
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
+
+// const main = () => {
+
+//   const randomNumber1 = Math.floor(Math.random() * 100 + 1 )
+//   const randomNumber2 = Math.floor(Math.random() * 100 + 1 )
+//   const randomNumber3 = Math.floor(Math.random() * 100 + 1 )
+
+//   const strResult = myFunction(randomNumber1, randomNumber2, randomNumber3)
+
+//   strResult.then()
+
+// }
