39d9b1a596afbfca97247a1f3611e5471e79f136
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-12-03 16:17:04 -0300

    exercise 3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Exercicio 1 =============================================
+
 const myFunction = (param1, param2, param3) => {
   return new Promise((resolve, reject) => {
     if (
@@ -39,6 +41,8 @@ console.log("função myFunction declarada");
 
 // console.log("exemplo 3");
 
+// Exercicio 2 =========================================================
+
 // função para criar um número aleatório entre 0 e 100
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 100 + 1);
@@ -65,4 +69,22 @@ const callMyFunction = () => {
 
 callMyFunction();
 
-console.log("ultimo teste");
+// Exercicio 3 ======================================================
+// Reescreva o código do exercício anterior para que utilize async/await.
+
+const callMyFunction2 = async () => {
+  const numbers = randomArrayNumbers();
+  console.log("numbers2: ", numbers);
+  console.log("com async/await");
+  // A sintaxe Try/catch deve ser utilizada para o tratamento de erros
+  try {
+    // Para isso lembre-se que é necessário atribuir o resultado da função a uma variável, nesse caso fora utilizado "result"
+    const result = await myFunction(...numbers);
+    // Considerando que o retorno do "resolve" da promisse em questão é um string, basta utilizar um console.log para a variável "result"
+    console.log(result);
+  } catch (batatinha) {
+    console.error(batatinha);
+  }
+};
+
+callMyFunction2();
